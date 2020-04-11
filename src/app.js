a799333026b40602d96597f5ab142c56c112c853
Caubert Castro <caubert.castro@gmail.com>
2020-04-10 22:02:28 -0300

    Primeiro desafio goStack

---
+++ src/app.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const cors = require("cors");
+
+const { uuid } = require("uuidv4");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+function checkRepositoryExists(request, response, next) {
+  const { id } = request.params;
+  const repository = repositories.find((repository) => repository.id === id);
+
+  if (!repository)
+    return response.status(400).json({ error: "Repository not found!" });
+
+  return next();
+}
+
+app.get("/repositories", (request, response) => {
+  return response.json(repositories);
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body;
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
+
+  repositories.push(repository);
+
+  return response.json(repository);
+});
+
+app.put("/repositories/:id", checkRepositoryExists, (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repository = repositories.find((repository) => repository.id === id);
+
+  repository.title = title;
+  repository.url = url;
+  repository.techs = techs;
+
+  return response.json(repository);
+});
+
+app.delete("/repositories/:id", checkRepositoryExists, (request, response) => {
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
+});
+
+app.post(
+  "/repositories/:id/like",
+  checkRepositoryExists,
+  (request, response) => {
+    const { id } = request.params;
+
+    const repository = repositories.find((repository) => repository.id === id);
+
+    repository.likes = ++repository.likes;
+    return response.json(repository);
+  }
+);
+
+module.exports = app;
